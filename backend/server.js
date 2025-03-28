22bcfd27abca32f2dd42fe74528a427200131f8d
Amha Mersha <amhamersh@gmail.com>
2025-03-28 13:04:25 +0300

    Submitted

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,12 @@ const encryptionRouter = require("./routes/encryptionRoute");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api/encryption", encryptionRouter);
 
-// Error handling
-//app.use((err, req, res, next) => {
-//  console.error(err.stack);
-//  res.status(500).json({ error: "Something went wrong!" });
-//});
-
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Encryption service running on port ${PORT}`);
